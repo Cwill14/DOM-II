b0700af71d51f66852e8c7d3d266dee8e3f2f766
Clark Williams <theclarkwilliams@gmail.com>
2019-05-14 15:35:26 -0600

    added load event

--- js/index.js
+++ js/index.js
@@ -9,8 +9,25 @@ nav.addEventListener('click', event => {
     event.target.style.color = "#212529";
 })
 //                  3 (not working)
-let signupBtn = document.querySelector('.btn');
-signupBtn.addEventListener('mouseover', event => {
-    event.target.setAttribute('transform', 'scale(1.15)');
-});
+// let signupBtns = document.querySelectorAll('.btn');
+// for(let i = 0; i < signupBtns.length; i++) {
+//     signupBtns[i].addEventListener('mouseover', event => {
+//         event.target[i].setAttribute('transform', 'scale(1.15)');
+//     });
+// };
+
+// signupBtn.addEventListener('mouseover', event => {
+//     event.target.setAttribute('transform', 'scale(1.15)');
+// });
+
 //                  4
+// let body = document.querySelector('body');
+// body.addEventListener('load', event => {
+//     event.target.setAttribute
+// })
+//                  5
+let busImg = document.querySelector('.intro img')
+busImg.addEventListener('load', event => {
+    event.target.style.border = "1px dashed red";
+});
+//                  6
