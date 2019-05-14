9d3ba06635e0e948542974b2a83fd846c85e052c
Clark Williams <theclarkwilliams@gmail.com>
2019-05-14 14:50:21 -0600

    got dblclick and click events working, struggling with others

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+//                  1
+let nav = document.querySelector('.nav');
+nav.addEventListener('dblclick', event => {
+    event.target.style.color = "blue";
+});
+//                  2
+nav.addEventListener('click', event => {
+    event.target.style.color = "#212529";
+})
+//                  3 (not working)
+let signupBtn = document.querySelector('.btn');
+signupBtn.addEventListener('mouseover', event => {
+    event.target.setAttribute('transform', 'scale(1.15)');
+});
+//                  4
